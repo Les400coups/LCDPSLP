186240f51454ae53b3fff18d125c038a6d567f9b
Les400coups <les400coups78@gmail.com>
2025-06-11 19:02:12 +0200

    1er

---
+++ bio.js
@@ -0,0 +1,7 @@
+const bios = {
+  nina: "Chanteuse et professeure de Comédie musicale.",
+  eric: "Improvisateur chevronné et blagueur invétéré.",
+  fred: "Comédien assuré et passionné.",
+  lilou: "Improvisatrice de talent et professeure de théâtre.",
+
+};
---
+++ events_tableau_complet.js
@@ -0,0 +1,43 @@
+
+const events_tableau = [
+
+/*
+  <span style='color:red'> pour la couleur
+  <br> saut de ligne
+ <a href='https://www.leboucanier.net' target='_blank'>www.leboucanier.net</a> pour lien direct
+*/
+
+
+  {
+    id: "26-06-2025",
+    titre: "J'aime beaucoup ce que vous faites.",
+    date: "Le 26 Juin 2025 à 20h30",
+    date_iso: "2025-06-26",
+    lieu: "Espace Bosco - Vernouillet",
+    description: "Comment une fausse manœuvre avec un téléphone portable vous fait découvrir ce que vos meilleurs amis pensent de vous en réalité, juste avant leur arrivée pour un week-end finalement pas comme les autres dans votre maison de campagne... <br><a href='mailto:reservation@courantdart.net'>reservation@courantdart.net</a>",
+    affiche: "images/Affiches/20250624.png",
+        highlight: true
+
+  },
+  {
+    id: "27-06-2025",
+    titre: "Les 400 Coups au Boucanier",
+    date: "27 juin 2025 à 21h",
+    date_iso: "2025-06-27",
+    lieu: "<span style='color:red'>Le Boucanier</span>  <br>Allée des Iles Tabarly <br>78200 Mantes-la-Jolie. <br>Restaurant dès 19h30. <br>Réservation :<br> <a href='https://www.leboucanier.net' target='_blank'>www.leboucanier.net</a><br> 01.30.63.70.30",
+    description: "Improviser, c'est possible. <br>Chanter, c'est possible. <br>Mais improviser en chantant ? <br> Bah c'est possible !",
+    affiche: "images/Affiches/affiche_27_06_2025.jpg",
+        highlight: false
+ 
+  }
+  
+  
+  
+  
+  
+  
+  
+];
+
+window.events_tableau_complet = events_tableau;
+window.events_tableau = events_tableau;
